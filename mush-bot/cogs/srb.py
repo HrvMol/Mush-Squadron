35078f60151d09f2f30e1a52f8a0c23fbeba7863
Harvin Molyneux <84520213+Joxnt@users.noreply.github.com>
2023-11-27 13:39:08 +0000

    Update srb.py
    changed class name

--- mush-bot/cogs/srb.py
+++ mush-bot/cogs/srb.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 import datetime
 from discord.commands import slash_command
 
-class MyCog(commands.Cog):
+class Srb(commands.Cog):
     def __init__(self, bot:commands.Bot):
         self.bot = bot
 
