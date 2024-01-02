75cd70878e7b77d97c518142e7acfa369c74051b
Harvin Molyneux <84520213+HrvMol@users.noreply.github.com>
2024-01-02 12:58:45 +0000

    decoration

--- mush-website/src/components/Footer.jsx
+++ mush-website/src/components/Footer.jsx
@@ -3,6 +3,6 @@ import React from 'react'
 export const Footer = () => (
     <section className='absolute left-0 right-0 ml-auto mr-auto w-fit bottom-0 font-karla text-[24px] text-center dark:text-custom-white text-custom-black uppercase'>
         <p>© 2023</p>
-        <a href='https://molyneuxtech.com/'>MADE BY MOLYNEUX TECH</a>
+        <a href='https://molyneuxtech.com/' className='text-[18px]'>MADE BY MOLYNEUX TECH</a>
     </section>
 )
--- mush-website/src/components/Table.jsx
+++ mush-website/src/components/Table.jsx
@@ -6,26 +6,39 @@ const data = [
     { username: "wall763abrasive", points: 542, activity: "381", joindate: "6/5/2021" },
     { username: "MrTacoDoesGames", points: 258, activity: "270", joindate: "20/5/2022" },
 ]
+
+var prevCol = ''
+const sortCols = (col) => {
+    var orderAsc = true
+    if (prevCol = col) {
+        orderAsc = false
+        console.log(col + ' descending');
+    }
+    prevCol = col
+
+    console.log(col + ' ascending');
+   };
+
 // table to be displayed of the data for the members in the squadron
 const Table = () => {
   return (
-    <table>
+    <table className='w-[80vw]'>
         {/* header element */}
-        <tr>
-            <th>Username</th>
-            <th>SRB Points</th>
-            <th>Activity</th>
-            <th>Join Date</th>
-        </tr>
+        <thead>
+            <td className='border-2 border-custom-black bg-custom-red' onClick={() => sortCols('username')}>Username</td>
+            <td className='border-2 border-custom-black bg-custom-red' onClick={() => sortCols('points')}>SRB Points</td>
+            <td className='border-2 border-custom-black bg-custom-red' onClick={() => sortCols('activity')}>Activity</td>
+            <td className='border-2 border-custom-black bg-custom-red' onClick={() => sortCols('joindate')}>Join Date</td>
+        </thead>
         {/* mapping the data to create a row for each member and inputting their data */}
         {data.map((val, key) => {
             return (
-                <tr key={key}>
-                    <td>{val.username}</td>
-                    <td>{val.points}</td>
-                    <td>{val.activity}</td>
-                    <td>{val.joindate}</td>
-                </tr>
+                <tbody key={key}>
+                    <td className='border-2 border-custom-black'>{val.username}</td>
+                    <td className='border-2 border-custom-black'>{val.points}</td>
+                    <td className='border-2 border-custom-black'>{val.activity}</td>
+                    <td className='border-2 border-custom-black'>{val.joindate}</td>
+                </tbody>
             )
         })}
     </table>
